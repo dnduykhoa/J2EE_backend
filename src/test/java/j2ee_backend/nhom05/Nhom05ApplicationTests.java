e4f4c85d9bd9cecc452df9f7ee5801b4048eb63c
Đào Khoa <dn.duykhoa@gmail.com>
2026-02-02 13:48:36 +0700

    Initial commit

---
+++ src/test/java/j2ee_backend/nhom05/Nhom05ApplicationTests.java
@@ -0,0 +1,13 @@
+package j2ee_backend.nhom05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nhom05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
